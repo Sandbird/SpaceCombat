cebaea31588e07aee18fc1a0f21d860ba0c37c14
blurboy1985 <daniel.tianwen@gmail.com>
2012-12-23 20:05:36 +0800

    Initial Commit

---
+++ SpaceGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpaceGame
+//
+//  Created by Daniel Quek on 23/12/12.
+//  Copyright Daniel Quek 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
