318b6612f76a3a99eaa58297b5d40bbc85f91c25
blurboy1985 <daniel.tianwen@gmail.com>
2012-12-25 10:46:26 +0800

    added spaceship

--- SpaceGame/ActionLayer.m
+++ SpaceGame/ActionLayer.m
@@ -13,6 +13,9 @@
     CCLabelBMFont *_titleLabel1;
     CCLabelBMFont *_titleLabel2;
     CCMenuItemLabel *_playItem;
+    CCSpriteBatchNode *_batchNode;
+    CCSprite *_ship;
+    float _shipPointsPerSecY;
 }
 
 +(id)scene {
@@ -27,6 +30,28 @@
     [sender removeFromParent];
 }
 
+-(void)spawnShip {
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    _ship = [CCSprite spriteWithSpriteFrameName:@"SpaceFlier_sm_1.png"];
+    _ship.position = ccp(-_ship.contentSize.width/2,
+                         winSize.height * 0.5);
+    [_batchNode addChild:_ship z:1];
+    
+    [_ship runAction:[CCSequence actions:
+                      [CCEaseOut actionWithAction:[CCMoveBy actionWithDuration:0.5 position:ccp(_ship.contentSize.width/2 + winSize.width *0.3, 0)] rate:4.0],
+                      [CCEaseInOut actionWithAction:[CCMoveBy actionWithDuration:0.5 position:ccp(-winSize.width *0.2, 0)] rate:4.0],
+                      nil]];
+    
+    CCSpriteFrameCache *cache = [CCSpriteFrameCache sharedSpriteFrameCache];
+    CCAnimation *animation = [CCAnimation animation];
+    [animation addSpriteFrame:[cache spriteFrameByName:@"SpaceFlier_sm_1.png"]];
+    [animation addSpriteFrame:[cache spriteFrameByName:@"SpaceFlier_sm_2.png"]];
+    animation.delayPerUnit = 0.2;
+    
+    [_ship runAction:[CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:animation]]];
+}
+
 -(void)playTapped:(id)sender {
     [[SimpleAudioEngine sharedEngine]playEffect:@"powerup.caf"];
     NSArray *nodes = @[_titleLabel1, _titleLabel2, _playItem];
@@ -36,6 +61,8 @@
                          [CCCallFuncN actionWithTarget:self selector:@selector(removeNode:)],
                          nil]];
     }
+    
+    [self spawnShip];
 }
 
 -(void)setupTitle {
@@ -104,6 +131,12 @@
     }
 }
 
+-(void)setupBatchNode {
+    _batchNode = [CCSpriteBatchNode batchNodeWithFile:@"Sprites.pvr.ccz"];
+    [self addChild:_batchNode z:-1];
+    [[CCSpriteFrameCache sharedSpriteFrameCache]addSpriteFramesWithFile:@"Sprites.plist"];
+}
+
 - (id)init
 {
     self = [super init];
@@ -111,8 +144,56 @@
         [self setupTitle];
         [self setupSound];
         [self setupStars];
+        [self setupBatchNode];
+        [self setAccelerometerEnabled:YES];
+        [self scheduleUpdate];
     }
     return self;
+}
+
+-(void)updateShipPos:(ccTime)dt
+{
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    float maxY = winSize.height - _ship.contentSize.height/2;
+    float minY = _ship.contentSize.height/2;
+    
+    float newY = _ship.position.y + (_shipPointsPerSecY * dt);
+    newY = MIN(MAX(newY, minY), maxY);
+    _ship.position = ccp(_ship.position.x, newY);
+}
+
+- (void)update:(ccTime)dt {
+    [self updateShipPos:dt];
+}
+
+#pragma mark - Apple Sample code for accelerometer
+
+- (void)accelerometer:(UIAccelerometer *)accelerometer
+        didAccelerate:(UIAcceleration *)acceleration {
+#define kFilteringFactor 0.75
+    static UIAccelerationValue rollingX = 0, rollingY = 0, rollingZ = 0;
+    rollingX = (acceleration.x * kFilteringFactor) +
+    (rollingX * (1.0 - kFilteringFactor));
+    rollingY = (acceleration.y * kFilteringFactor) +
+    (rollingY * (1.0 - kFilteringFactor));
+    rollingZ = (acceleration.z * kFilteringFactor) +
+    (rollingZ * (1.0 - kFilteringFactor));
+    float accelX = rollingX;
+    float accelY = rollingY;
+    float accelZ = rollingZ;
+//    NSLog(@"accelX: %f, accelY: %f, accelZ: %f",
+//          accelX, accelY, accelZ);
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+#define kRestAccelX 0.6
+#define kShipMaxPointsPerSec (winSize.height*0.5)
+#define kMaxDiffX 0.2
+    float accelDiffX = kRestAccelX - ABS(accelX);
+    float accelFractionX = accelDiffX / kMaxDiffX;
+    float pointsPerSecX = kShipMaxPointsPerSec * accelFractionX;
+    _shipPointsPerSecY = pointsPerSecX;
 }
 
 @end
